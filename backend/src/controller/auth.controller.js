21239f47cce64ee8086aa0c6fadb81be19e259d0
KalashT <kalashthakare898@gmail.com>
2025-02-23 12:03:29 +0530

    login logout completed

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -55,21 +55,44 @@ export const signup = async (req,res) => {
 };
 
 
-export const login = (req,res) => {
-    const {username,password} = req.body;
+export const login = async (req,res) => {
+    const {email,password} = req.body;
     try {
-        
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        const checkPassword =  await bcrypt.compare(password,user.password);
+
+        if(!checkPassword){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email,
+            profilePic:user.profilepic
+        });
+
+
     } catch (error) {
-        
+        console.log("Error in login controller-",error.message);
+        res.status(500).json({message:"Internal server error"});
     }
 };
 
 
 export const logout = (req,res) => {
-    const {username,password} = req.body;
     try {
-        
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
     } catch (error) {
-        
+
+        console.log("Error in logout controller-",error.message);
+        res.status(500).json({message:"Internal server error"});
     }
 }
