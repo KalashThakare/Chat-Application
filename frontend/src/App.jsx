c9d973aeed6397d2ef2afd9bfe7644c79821aec5
KalashT <kalashthakare898@gmail.com>
2025-02-24 11:15:06 +0530

    created pages and axiosInstance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from './components/Navbar';
+import {Route, Routes} from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
+
   return (
-    <div className=''>hello world</div>
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignupPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/settings" element={<SettingsPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
+      
+      
+    </div>
   )
 }
 
