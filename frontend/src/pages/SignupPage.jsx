ca169ae715d567558b2541af8d3c84a1601cfc7c
KalashT <kalashthakare898@gmail.com>
2025-02-25 22:24:23 +0530

    Signup ui completed

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuth.store';
-import { MessageSquare } from 'lucide-react';
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, MessagesSquareIcon, User } from 'lucide-react';
+import {Link} from "react-router-dom"
 
 const SignupPage = () => {
-  const {showPassword,setPassword} = useState(false);
-  const {formData,setFormData} = useState({
+  const [showPassword,setShowPassword] = useState(false);
+  const [formData,setFormData] = useState({
     fullName:"",
     email:"",
     password:""
@@ -30,16 +31,92 @@ const SignupPage = () => {
         <div className='w-full max-w-md space-y-8'>
 
         <div className="text-center mb-8">
+
             <div className="flex flex-col items-center gap-2 group">
               <div
                 className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
               group-hover:bg-primary/20 transition-colors"
               >
-                <MessageSquare className="size-6 text-primary" />
+                <MessagesSquareIcon className="size-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
               <p className="text-base-content/60">Get started with your free account</p>
             </div>
+
+            <form onSubmit={handleSubmit} className='space-y-6'>
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text font-medium'>Full Name</span>
+                </label>
+                <div className='relative'>
+                  <div className='absolute inset-y-0 left-0 pl-3 flex pointer-events-none items-center'>
+                    <User className='size-5 text-base-content/40 '/>
+                  </div>
+                  <input type="text" 
+                    placeholder='Enter name'
+                    className='input input-bordered w-full pl-10'
+                    value={formData.fullName}
+                    onChange={(e)=> setFormData({...formData, fullName : e.target.value})}
+                  />
+                </div>
+              </div>
+
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text font-medium'>Email</span>
+                </label>
+                <div className='relative'>
+                  <div className='absolute inset-y-0 left-0 pl-3 flex pointer-events-none items-center'>
+                    <Mail className='size-5 text-base-content/40 '/>
+                  </div>
+                  <input type="text" 
+                    placeholder='Email'
+                    className='input input-bordered w-full pl-10'
+                    value={formData.email}
+                    onChange={(e)=> setFormData({...formData, email : e.target.value})}
+                  />
+                </div>
+              </div>
+
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text font-medium'>Password</span>
+                </label>
+                <div className='relative'>
+                  <div className='absolute inset-y-0 left-0 pl-3 flex pointer-events-none items-center'>
+                    <Lock className='size-5 text-base-content/40 '/>
+                  </div>
+                  <input type={showPassword ? "text" : "password"} 
+                    placeholder='Enter your password'
+                    className='input input-bordered w-full pl-10'
+                    value={formData.password}
+                    onChange={(e)=> setFormData({...formData, password : e.target.value})}
+                  />
+                  <button type='button' 
+                    className='absolute inset-y-0 right-0 pr-3 flex items-center'
+                    onClick={()=> setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? 
+                    ( <EyeOff className='size-5 text-base-content/40' />) : (<Eye className='size-5 text-base-content/40'/>)
+                    }
+                  </button>
+                </div>
+              </div>
+
+              <button type='submit' className='btn btn-primary w-full' disabled={isSigningUp}>
+                {isSigningUp ? (<> <Loader2 className='size-5 animate-spin'/> Loading... </>) : ("Create Account")}
+              </button>
+            </form>
+
+            <div className='text-center mt-2'>
+              <p className='text-base-content/60'>
+                Alredy have an account?{" "}
+                <Link to="/login" className="link link-primary">
+                  Sign in
+                </Link>
+              </p>
+            </div>
+
           </div>
 
         </div>
