761f28588f230b90c0eca75fc763691ad8f35787
KalashT <kalashthakare898@gmail.com>
2025-02-28 22:36:42 +0530

    online users check real time

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -2,6 +2,7 @@ import {Server} from "socket.io";
 import express from "express";
 import http from "http";
 import { Socket } from "dgram";
+import { sourceMapsEnabled } from "process";
 
 const app = express();
 const server = http.createServer(app);
@@ -12,11 +13,25 @@ const io = new Server(server,{
     },
 });
 
+const userSocketMap = {}
+
 io.on("connection",(Socket)=>{
     console.log("A user connected", Socket.id);
 
+    const userId = Socket.handshake.query.userId;
+
+    if(userId){
+        userSocketMap(userId)=Socket.id;
+    }
+
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
     Socket.on("disconnect",()=>{
         console.log("A user disconnected",Socket.id);
+
+        delete userSocketMap(userId);
+        
+        io.emit("getOnlineUsers",Object.keys(userSocketMap));
     });
 });
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -100,10 +100,18 @@ export const useAuthStore = create((set,get)=>({
         if(!authUser || get().socket?.connected){
             return;
         }
-        const socket =io("http://localhost:4000");
+        const socket =io("http://localhost:4000",{
+            query:{
+                userId:authUser._id
+            }
+        });
         socket.connect()
 
         set({socket:socket});
+
+        socket.on("getOnlineUsers" ,(userIds)=>{
+            set({onLineUsers:userIds});
+        });
     },
 
     disconnectSocket:()=>{
